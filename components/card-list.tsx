604e0f0bfa448b533379caa1a1572f2f21d76d21
Kevin Old <kevin@kevinold.com>
2023-10-28 08:46:31 -0500

    wip

---
+++ components/card-list.tsx
@@ -0,0 +1,18 @@
+import { Card } from "./ui/card";
+
+const cardList = [
+  {
+title: "Card Title",
+description: "Card Description",
+body: "Card Body",
+  }
+]
+
+
+export function CardList() {
+  <>
+  <Card>
+    <Card.Title>Card Title</Card.Title>
+  </Card>
+  </>
+}
